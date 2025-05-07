0ce4122aca9deb8a3d4a9310e4c3dcb6beb5f739
zia-bonzer <zia.bonzer@gmail.com>
2025-05-07 23:32:36 +0530

    adds dynamic UI loader from Layout AST JSON

---
+++ components/layout-renderer/components.ts
@@ -0,0 +1,15 @@
+import { ActivePackagesCard } from "@/components/organisms/patientComponents/ActivePackagesCard";
+import { CampaignsCard } from "@/components/organisms/patientComponents/CampaignsCard";
+import { MedicalSummaryCard } from "@/components/organisms/patientComponents/MedicalSummaryCard";
+import { NextAppointmentCard } from "@/components/organisms/patientComponents/NextAppointmentCard";
+import { RecentDocumentsCard } from "@/components/organisms/patientComponents/RecentDocumentsCard";
+import { UserHistoryCard } from "@/components/organisms/patientComponents/UserHistoryCard";
+
+export const componentMap: Record<string, React.ComponentType<any>> = {
+  UserHistoryCard,
+  RecentDocumentsCard,
+  ActivePackagesCard,
+  CampaignsCard,
+  NextAppointmentCard,
+  MedicalSummaryCard,
+};
